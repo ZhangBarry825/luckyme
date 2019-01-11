0ba21c432ffe0146bd733e580c3fc325ecde9c82
zhangbin <530027054@qq.com>
2019-01-11 14:15:56 +0800

    修改日期显示问题

--- plugins/Methods.js
+++ plugins/Methods.js
@@ -1,46 +1,46 @@
 
 function timeFormat(data) {
   //三月 31 , 2018
-  let time1=Date.parse(data)
+  let time1=Date.parse(data);
   var date = new Date(time1 );
-  var Y = date.getFullYear()
-  var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1)
+  var Y = date.getFullYear();
+  var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1);
   switch (M) {
-    case 1:
-      M='一月 , '
+    case '01':
+      M='一月 , ';
       break;
-    case 2:
-      M='二月 , '
+    case '02':
+      M='二月 , ';
       break;
-    case 3:
-      M='三月 , '
+    case '03':
+      M='三月 , ';
       break;
-    case 4:
-      M='四月 , '
+    case '04':
+      M='四月 , ';
       break;
-    case 5:
-      M='五月 , '
+    case '05':
+      M='五月 , ';
       break;
-    case 6:
-      M='六月 , '
+    case '06':
+      M='六月 , ';
       break;
-    case 7:
-      M='七月 , '
+    case '07':
+      M='七月 , ';
       break;
-    case 8:
-      M='八月 , '
+    case '08':
+      M='八月 , ';
       break;
-    case 9:
-      M='九月 , '
+    case '09':
+      M='九月 , ';
       break;
-    case 10:
-      M='十月 , '
+    case '10':
+      M='十月 , ';
       break;
-    case 11:
-      M='冬月 , '
+    case '11':
+      M='冬月 , ';
       break;
-    case 12:
-      M='十二月 , '
+    case '12':
+      M='十二月 , ';
       break;
 
   }
